6464ab33a323b3bc1339cc03322471b5562dfae0
Eric Florenzano <floguy@gmail.com>
2017-09-26 02:24:55 -0700

    Work on the homepage

--- siacdn-www/pages/index.js
+++ siacdn-www/pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import cookies from 'next-cookies';
-import { Segment, Grid, Item, Header, Button, List } from 'semantic-ui-react';
+import { Segment, Grid, Item, Header, Button, List, Message } from 'semantic-ui-react';
 import Nav from '../components/nav';
 import Client from '../lib/client';
 
@@ -18,29 +18,38 @@ const Index = ({ authAccount }) => (
     <div className="holder">
       <Nav activeItem="index" authAccount={authAccount} />
       <Segment padded>
-        <Header as="h1">SiaCDN</Header>
-        <p>
+        <Header as='h2'>
           <strong>SiaCDN</strong> is the easiest way to get started with Sia in
           the cloud.
-        </p>
-        <p>
-          We spin up a full Sia node for you, along with a specialized version
-          of Minio which provides an S3-compatible frontend to your Sia full
-          node.
-        </p>
+          <Header.Subheader>
+            We host a Sia full node for you, along with a specialized version
+            of Minio that provides an S3-compatible API to use Sia.
+          </Header.Subheader>
+        </Header>
+
+        <Message positive><Message.Header>If you&rsquo;re interested in supporting distributed systems...</Message.Header>
+            Increasing overall network usage is the real best way to show your support.
+            We believe that if it&rsquo;s easy to get started with Sia — if,
+            within an hour of hearing about it, a developer who has used Amazon
+            Web Services could use it, then overall network usage will skyrocket.
+            We believe SiaCDN achieves this, and we hope you support us in
+            supporting the Sia network.</Message>
 
         <Header as="h2">Price</Header>
         <p>
-          We charge a flat fee for our services of <strong>$30/month</strong>{' '}
-          per full Sia node, <strong>$10/month</strong> per Minio instance, and{' '}
-          <strong>$0.03/GB</strong> bandwidth out. This is more than we&rsquo;d
-          like to charge, but it&rsquo;s because we currently have to proxy all
-          traffic and pay those bandwidth costs. In the future we will be able
-          to cut this down dramatically when the Sia network{' '}
-          <strong>adds CDN functionality</strong>. Note that you cannot buy any
-          SiaCoins from us - you are paying for our hosting services and we use
-          the Sia network to provide those services to you.
+          We charge a flat fee for our services of <strong>$10/month</strong>{' '}
+          per Sia full node, <strong>$10/month</strong> per Minio instance,{' '}
+          <strong><a href="https://siastats.info/storage_pricing.html" target="_blank">$1.20/TB</a></strong> of Sia network storage and{' '}
+          <strong><a href="https://cloud.google.com/compute/pricing#internet_egress" target="_blank">$0.025/GB</a></strong> of bandwidth out. This is more than
+          we&rsquo;d like to charge, but it&rsquo;s because we currently have to
+          proxy all traffic and pay those bandwidth costs. In the future we will
+          reduce the costs substantially by offloading the majority
+          of the work to the Sia network itself (once it has a few more
+          features.)
         </p>
+        <Message info>Note that you cannot buy virtual currency of any kind from us - you are
+        paying for our internet hosting service, and we use the Sia network to provide
+        that service to you.</Message>
 
         <Header as="h2">Steps</Header>
         <Grid columns={3} divided>
