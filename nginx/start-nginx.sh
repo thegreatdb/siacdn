d81a64f8ba6b04e7bedbcf36c94b198b18ff2ab5
Eric Florenzano <floguy@gmail.com>
2020-02-18 08:51:48 -1000

    First pass at updating nginx, viewnode, and new portal setup

---
+++ nginx/start-nginx.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+APIPASSWORD=`cat /root/.sia/apipassword`
+BASE64_AUTHENTICATION=`echo ":$APIPASSWORD" | base64 -`
+cat /etc/skynet/nginx.conf | sed -- 's#'"BASE64_AUTHENTICATION"'#'"$BASE64_AUTHENTICATION"'#g' - > /etc/nginx/conf.d/nginx.conf
+
+nginx -g "daemon off;"
