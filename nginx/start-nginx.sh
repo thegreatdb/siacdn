6b1bb26bf30b00fe6ae356cf298530c49d416606
Eric Florenzano <floguy@gmail.com>
2020-03-18 20:33:09 -0700

    Attempt to send reads and writes to different siads

--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-APIPASSWORD=`cat /root/.sia/apipassword`
-BASE64_AUTHENTICATION=`echo -n ":$APIPASSWORD" | base64 -`
-cat /etc/skynet/nginx.conf | sed -- 's#'"BASE64_AUTHENTICATION"'#'"$BASE64_AUTHENTICATION"'#g' - > /etc/nginx/conf.d/nginx.conf
+READ_APIPASSWORD=`cat /root/.sia/apipassword`
+WRITE_APIPASSWORD=`cat /root/.sia-upload/apipassword`
+READ_BASE64_AUTHENTICATION=`echo -n ":$READ_APIPASSWORD" | base64 -`
+WRITE_BASE64_AUTHENTICATION=`echo -n ":$WRITE_APIPASSWORD" | base64 -`
+cat /etc/skynet/nginx.conf | \
+    sed -- 's#'"READ_BASE64_AUTHENTICATION"'#'"$READ_BASE64_AUTHENTICATION"'#g' - | \
+    sed -- 's#'"WRITE_BASE64_AUTHENTICATION"'#'"$WRITE_BASE64_AUTHENTICATION"'#g' - > /etc/nginx/conf.d/nginx.conf
 
 nginx -g "daemon off;error_log /dev/stdout info;"
