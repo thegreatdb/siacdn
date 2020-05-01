ded398008eb1e04ae535c3884764cac36ee49f2a
Eric Florenzano <floguy@gmail.com>
2020-05-01 13:08:02 -1000

    Get rid of bootstrap stuff for now, and rearrange Docker instruction order to make changes less data intensive

--- viewnode/siad-wrapper.sh
+++ viewnode/siad-wrapper.sh
@@ -9,20 +9,20 @@ source /go/bin/setup-env.sh
 echo "Starting up with SIAD_DATA_DIR as $SIAD_DATA_DIR"
 #echo "Bootstrapping wallet with password: $SIA_WALLET_PASSWORD"
 
-if [ ! -f consensus/consensus.db ]; then
-    echo "Found no consensus.db, downloading now..."
-    curl https://siastats.info/bootstrap/bootstrap.zip -o /tmp/bootstrap.zip
-    echo "Finished downloading consensus.db, extracting now..."
-    unzip /tmp/bootstrap.zip -d /etc/sia
-    echo "Finished bootstrapping consensus.db"
-fi
+#if [ ! -f consensus/consensus.db ]; then
+#    echo "Found no consensus.db, downloading now..."
+#    curl https://siastats.info/bootstrap/bootstrap.zip -o /tmp/bootstrap.zip
+#    echo "Finished downloading consensus.db, extracting now..."
+#    unzip /tmp/bootstrap.zip -d /etc/sia
+#    echo "Finished bootstrapping consensus.db"
+#fi
 
-if [ ! -f renter/hostdb.json ]; then
-    echo "Found no hostdb.json, downloading now..."
-    curl https://siastats.info/bootstrap/hostdb.zip -o /tmp/hostdb.zip
-    echo "Finished downloading hostdb.json, extracting now..."
-    unzip /tmp/hostdb.zip -d /etc/sia
-    echo "Finished bootstrapping hostdb.json"
-fi
+#if [ ! -f renter/hostdb.json ]; then
+#    echo "Found no hostdb.json, downloading now..."
+#    curl https://siastats.info/bootstrap/hostdb.zip -o /tmp/hostdb.zip
+#    echo "Finished downloading hostdb.json, extracting now..."
+#    unzip /tmp/hostdb.zip -d /etc/sia
+#    echo "Finished bootstrapping hostdb.json"
+#fi
 
 /go/bin/siad -d /etc/sia "$@"
