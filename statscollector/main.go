742ff4e08162b905ecfbedf9538695f930d54614
Eric Florenzano <floguy@gmail.com>
2020-05-06 19:44:50 -1000

    Collect viewnode data too

--- statscollector/main.go
+++ statscollector/main.go
@@ -31,18 +31,29 @@ type Stats struct {
 	VersionInfo      StatsVersions `json:"versioninfo"`
 	PerformanceStats interface{}   `json:"performancestats,omitempty"`
 	Uploaders        interface{}   `json:"uploaders,omitempty"`
+	Viewers          interface{}   `json:"viewers,omitempty"`
 }
 
 var statsMux sync.RWMutex
-var collectedStats map[string]Stats = make(map[string]Stats, 0)
+var collectedUploadStats map[string]Stats = make(map[string]Stats, 0)
+var collectedViewStats map[string]Stats = make(map[string]Stats, 0)
 
 func serveAggregatedStats(w http.ResponseWriter, r *http.Request) {
 	var versionInfo *StatsVersions = nil
 	var aggregatedTotals StatsTotals
 	uploaders := make(map[string]Stats, 0)
+	viewers := make(map[string]Stats, 0)
 	statsMux.RLock()
-	for name, stats := range collectedStats {
+	for name, stats := range collectedUploadStats {
 		uploaders[name] = stats
+		if versionInfo == nil {
+			versionInfo = &stats.VersionInfo
+		}
+		aggregatedTotals.NumFiles += stats.UploadStats.NumFiles
+		aggregatedTotals.TotalSize += stats.UploadStats.TotalSize
+	}
+	for name, stats := range collectedViewStats {
+		viewers[name] = stats
 		if versionInfo == nil {
 			versionInfo = &stats.VersionInfo
 		}
@@ -57,6 +68,7 @@ func serveAggregatedStats(w http.ResponseWriter, r *http.Request) {
 	}
 	resp := map[string]interface{}{
 		"uploaders":   uploaders,
+		"viewers":     viewers,
 		"uploadstats": aggregatedTotals,
 		"versioninfo": versionInfo,
 	}
@@ -94,31 +106,48 @@ func collectAll() {
 		log.Println("Could not set up kubernetes client from configuration")
 		return
 	}
-	pods, err := clientset.CoreV1().Pods("default").List(metav1.ListOptions{
+
+	uploaderPods, err := clientset.CoreV1().Pods("default").List(metav1.ListOptions{
 		LabelSelector: "app=siacdn-uploader",
 	})
 	if err != nil {
-		log.Println("Could not list pods from kubernetes")
+		log.Println("Could not list uploader pods from kubernetes")
 		return
 	}
+
+	viewerPods, err := clientset.CoreV1().Pods("default").List(metav1.ListOptions{
+		LabelSelector: "app=siacdn-viewer",
+	})
+	if err != nil {
+		log.Println("Could not list uploader pods from kubernetes")
+		return
+	}
+
 	var wg sync.WaitGroup
-	for _, pod := range pods.Items {
+	for _, pod := range uploaderPods.Items {
 		wg.Add(1)
 		go func(name, ip string) {
 			defer wg.Done()
-			collectOne(name, ip)
+			collectOne(name, ip, "stats", collectedUploadStats)
+		}(pod.Name, pod.Status.PodIP)
+	}
+	for _, pod := range viewerPods.Items {
+		wg.Add(1)
+		go func(name, ip string) {
+			defer wg.Done()
+			collectOne(name, ip, "statsdown", collectedViewStats)
 		}(pod.Name, pod.Status.PodIP)
 	}
 	wg.Wait()
 	return
 }
 
-func collectOne(name, ip string) {
+func collectOne(name, ip, pathPart string, statMap map[string]Stats) {
 	log.Println("About to collect from", name)
 	var netClient = &http.Client{
 		Timeout: time.Second * 600,
 	}
-	resp, err := netClient.Get(fmt.Sprintf("http://%s:8080/stats", ip))
+	resp, err := netClient.Get(fmt.Sprintf("http://%s:8080/"+pathPart, ip))
 	if err != nil {
 		log.Println("Could not collect stats from", name, err)
 		//statsMux.Lock()
@@ -141,7 +170,7 @@ func collectOne(name, ip string) {
 		return
 	}
 	statsMux.Lock()
-	collectedStats[name] = stats
+	statMap[name] = stats
 	statsMux.Unlock()
 	log.Println("Got", stats.UploadStats.NumFiles, "files on", name)
 }
